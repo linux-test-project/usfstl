1a42e38ffab9b19282307f555c6f69bcb2b50e4e
Johannes Berg <johannes.berg@intel.com>
2021-04-28 10:04:34 +0200

    uds: add more documentation
    Change-Id: I0b9698e3e4bb9750219ef0f430abf30cb0011a5f

--- include/usfstl/uds.h
+++ include/usfstl/uds.h
@@ -13,8 +13,20 @@
 #include <stdint.h>
 #include "list.h"
 
+/**
+ * usfstl_uds_create - create a new unix domain socket server
+ * @path: the path to create the server at
+ * @connected: callback to be called when a new client connects,
+ *	called with the fd and the data
+ * @data: the data to pass to the @connected callback
+ */
 void usfstl_uds_create(const char *path, void (*connected)(int, void *),
 		       void *data);
+
+/**
+ * usfstl_uds_remove - remove a unix domain socket server
+ * @path: the path indicating which server to remove
+ */
 void usfstl_uds_remove(const char *path);
 
 /**
@@ -27,8 +39,24 @@ void usfstl_uds_remove(const char *path);
  */
 int usfstl_uds_connect_raw(const char *path);
 
+/**
+ * usfstl_uds_connect - make a unix domain socket connection
+ * @path: the path to connect to
+ * @readable: the callback when the fd becomes readable, called
+ *	with the fd and the data
+ * @data: the data to pass to the @readable callback
+ *
+ * This also adds the new connection to the usfstl loop and
+ * calls the readable handler (with the fd and data) when the
+ * fd becomes readable.
+ */
 int usfstl_uds_connect(const char *path, void (*readable)(int, void *),
 		       void *data);
+
+/**
+ * usfstl_uds_disconnect - disconnect a unix domain socket connection
+ * @fd: the fd for the connection
+ */
 void usfstl_uds_disconnect(int fd);
 
 #endif // _USFSTL_UDS_H_
