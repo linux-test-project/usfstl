c30addf0f19e33da3ccd18c2a05b85cac982c5ef
Benjamin Berg <benjamin.berg@intel.com>
2023-10-19 13:56:08 +0200

    vlab: return failure if a kernel memory leak was detected
    Use the new postrun hook in order to return a failure if a kernel memory
    leak was detected.
    
    Change-Id: I569f515c402a2303352b24b24f13797e1462aafe

--- vlab/plugins/kmemleak.py
+++ vlab/plugins/kmemleak.py
@@ -6,6 +6,7 @@
 """
 kmemleak checker plugin for VLAB
 """
+import glob
 import virtlab
 
 class KmemleakPlugin(virtlab.Plugin):
@@ -19,8 +20,15 @@ class KmemleakPlugin(virtlab.Plugin):
           echo scan > /sys/kernel/debug/kmemleak 2> /dev/null || echo "kmemleak is disabled on $HOSTNAME"
           sleep 5 # minimum age for reporting in kmemleak
           echo scan > /sys/kernel/debug/kmemleak 2> /dev/null
-          cat /sys/kernel/debug/kmemleak > /dev/console
+          cat /sys/kernel/debug/kmemleak > /tmp/kmemleak
+          test -s /tmp/kmemleak && touch $TMPDIR/kmemleak-leaked-$HOSTNAME
+          cat /tmp/kmemleak > /dev/console
         )
         '''
+
+    def postrun(self, runtime: virtlab.VlabRuntimeData) -> None:
+        hosts = [f[16:] for f in glob.glob('kmemleak-leaked-*', root_dir=runtime.tmpdir)]
+        if hosts:
+            raise virtlab.Failure(f'Kernel memory leak detected on hosts: {", ".join(hosts)}')
 
     ctrlstop = nodestop
