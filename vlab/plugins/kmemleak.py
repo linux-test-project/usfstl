a9ddec82fa09e379d9d9a2459357bacc204c22f0
Benjamin Berg <benjamin.berg@intel.com>
2024-09-27 12:00:54 +0200

    plugins: kmemleak: make kmemleak configurable
    The plugin is pretty pointless if kmemleak is not enabled. That said, it
    adds a lot of overhead so it really needs to be configurable.
    
    Add an option to enable kmemleak, if not set, disable it explicitly.
    Also add some common caches into the slub_debug= list.
    
    Change-Id: Ie47fc143db9f943ce5f1903e0ce21608a272f6f1

--- vlab/plugins/kmemleak.py
+++ vlab/plugins/kmemleak.py
@@ -7,12 +7,48 @@
 kmemleak checker plugin for VLAB
 """
 import glob
+from typing import List, Any, Optional
+
 import virtlab
+
+class KmemleakPluginNode(virtlab.PluginNode):
+    """
+    Kmemleak plugin per-node information
+    """
+    def __init__(self, plugin: virtlab.Plugin, cfg: Any) -> None:
+        super().__init__(plugin)
+
+        if isinstance(cfg, bool):
+            cfg = {} if cfg else None
+
+        self._cfg = cfg
+
+    def linux_cmdline(self, runtime: virtlab.VlabRuntimeData) -> List[str]:
+        # Explicitly enable/disable so that the kernel default does not matter
+        if self._cfg is None:
+            return [ 'kmemleak=off' ]
+
+        ret = ['kmemleak=on']
+        caches = [
+            'kmemleak_object',
+            'kmemleak_scan_area',
+            'names_cache',
+            'hostfs_inode_info',
+            'ovl_inode',
+        ]
+        if 'caches' in self._cfg:
+            caches.extend(self._cfg['caches'])
+        ret.extend([f'slub_debug=-,{",".join(caches)}'])
+        return ret
+
 
 class KmemleakPlugin(virtlab.Plugin):
     """
     kmemleak plugin for VLAB
     """
+    def parse_node(self, node: virtlab.Node, nodecfg: Any) -> Optional[KmemleakPluginNode]:
+        return KmemleakPluginNode(self, nodecfg.get('kmemleak', None))
+
     def nodestop(self, runtime: virtlab.VlabRuntimeData) -> str:
         return '''
         test -f /sys/kernel/debug/kmemleak && (
